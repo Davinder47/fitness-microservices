0070106ef149b936285856771f41f48a8818cec8
Davinder47 <davinder9730@gmail.com>
2026-02-27 23:58:19 +0530

    Added some comments in UserResponse

---
+++ userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -0,0 +1,6 @@
+package com.fitness.userservice.dto;
+
+//This is a DTO object
+//Here we will define what all fields we want to be part, like we want this particular class part
+public class UserResponse {
+}
