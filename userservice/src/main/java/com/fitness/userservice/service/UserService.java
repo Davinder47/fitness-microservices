1666fc6a758ae0d88619f9fc50ef7ebfeadec014
Davinder47 <davinder9730@gmail.com>
2026-02-28 13:18:37 +0530

    Created regiter method in UserService to register user into our application and then created UserRepository in repository package and updated UserService to get instance of repository

--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -1,7 +1,33 @@
 package com.fitness.userservice.service;
 
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import com.fitness.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+
+//Method where we will be registering the user into aur application
 public class UserService {
+
+    //Getting instance of repository
+    @Autowired
+    private UserRepository repository;
+
+    public UserResponse register(RegisterRequest request) {
+        //First we will create User object and set his things
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        repository.save(user);
+
+        //Now we will save the user into our database,
+        // we need repository layer for this i.e UserRepository(which is an interface)
+
+    }
 }
