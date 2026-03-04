4ad80a61de6eb7ade6717f3d645c5d3340a256c4
Davinder47 <davinder9730@gmail.com>
2026-03-04 11:56:54 +0530

    Created enum called ActivityType in same package model and added the activities

---
+++ activityservice/src/main/java/com/example/activityservice/model/ActivityType.java
@@ -0,0 +1,16 @@
+package com.example.activityservice.model;
+
+//ActivityType is a enum which defines the type of activity that our app allows
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+
+}
