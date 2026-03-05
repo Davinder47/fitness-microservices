007a0f6e3e5e4a58b20f80ff48c71e347bd83974
Davinder47 <davinder9730@gmail.com>
2026-03-05 17:30:33 +0530

    Setted up the ActivityService

--- activityservice/src/main/java/com/example/activityservice/ActivityRepository.java
+++ activityservice/src/main/java/com/example/activityservice/ActivityRepository.java
@@ -1,7 +1,13 @@
 package com.example.activityservice;
 
+import com.example.activityservice.model.Activity;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+//This repository will help us to save the data into the DB
+//MongoRepository is an interface which already exists
+//Activity is the model that it is representing
 @Repository
-public interface ActivityRepository {
+public interface ActivityRepository extends MongoRepository<Activity, String> {
+
 }
--- activityservice/src/main/java/com/example/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/example/activityservice/service/ActivityService.java
@@ -1,13 +1,50 @@
 package com.example.activityservice.service;
 
+import com.example.activityservice.ActivityRepository;
 import com.example.activityservice.dto.ActivityRequest;
 import com.example.activityservice.dto.ActivityResponse;
+import com.example.activityservice.model.Activity;
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor//to inject the activityRepository automatically injected
+//Above will only generate constructor with required arguments like: final and
+//fields with constraints such as @NonNull, whereas @AllArgsConstructor
+//generates constructor for every field in the class
 public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
     //When the user will say trackActivity, we will actually save it into DB
     public ActivityResponse trackActivity(ActivityRequest request) {
+
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration((request.getDuration()))
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(activity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity){
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(response.getStartTime());
+        response.setAdditionalMetrics(response.getAdditionalMetrics());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+
+        return response;
     }
 }
