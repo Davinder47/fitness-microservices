fcfd1a09912c91006807d0785f5296921175a291
Davinder47 <davinder9730@gmail.com>
2026-03-06 17:18:53 +0530

    Added a class called MongoConfig under config package for MongoAuditing for createdAt and updatedAt null error

--- activityservice/src/main/java/com/example/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/example/activityservice/ActivityserviceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.example.activityservice")
 public class ActivityserviceApplication {
 
 	public static void main(String[] args) {
