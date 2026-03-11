12c981b2cebd9ac1c633aa64253304cd0d1ec57c
Davinder47 <davinder9730@gmail.com>
2026-03-11 18:29:23 +0530

    Setted up Eureka Dashboard Server and added @EnableEurekaServer annotaion in main file and updated properties file

--- eureka/src/main/java/com/server/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/server/eureka/EurekaApplication.java
@@ -2,8 +2,11 @@ package com.server.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 //This is our third microservice
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
