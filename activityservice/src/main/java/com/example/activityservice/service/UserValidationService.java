eb4beb4dc1beb3ce6eab4814543038c6b982001a
Davinder47 <davinder9730@gmail.com>
2026-04-02 23:53:26 +0530

    Added the same log into UserValidationService too, to know the BTS of API calls

--- activityservice/src/main/java/com/example/activityservice/service/UserValidationService.java
+++ activityservice/src/main/java/com/example/activityservice/service/UserValidationService.java
@@ -4,17 +4,20 @@ package com.example.activityservice.service;
 //This service will interact with user-service and will actually validate user
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserValidationService {
     private final WebClient userServiceWebClient;
 
     public boolean validateUser(String userId){
+        log.info("Calling User Validation API for userId: {}", userId);
         try {
             return userServiceWebClient.get()
                     .uri("/api/users/{userId}/validate", userId)
