1c243d1d6b2908148df6c52e7e36cb6aae3690b0
Davinder47 <davinder9730@gmail.com>
2026-04-24 12:20:38 +0530

    Created endpoints in RecommendationController to get the Userrecommendation and Recommendation for a particular acrivity id

--- aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
+++ aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
@@ -1,12 +1,31 @@
 package com.fitness.aiservice.controller;
 
 
+import com.fitness.aiservice.model.Recommendation;
 import com.fitness.aiservice.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/recommendations")
 public class RecommendationController {
     private final RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")//to get the UserRecommendation
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String userId){
+        return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
+    }
+
+    @GetMapping("/user/{activityId}")//to get the Recommendation for a particular activity id
+    public ResponseEntity<List<Recommendation>> getActivityRecommendation(@PathVariable String activityId){
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+    }
+
 }
