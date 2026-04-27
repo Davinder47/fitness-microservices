21840e6deaee5e5746a5410b1d94e9fcce1a7736
Davinder47 <davinder9730@gmail.com>
2026-04-27 16:41:45 +0530

    Asynchronous communication established and messages are going into queue for one by one processing, reducing the load on application

--- activityservice/src/main/java/com/example/activityservice/config/RabbitMqConfig.java
+++ activityservice/src/main/java/com/example/activityservice/config/RabbitMqConfig.java
@@ -3,20 +3,45 @@ package com.example.activityservice.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
     @Bean
     public Queue activityQueue() {
-        return new Queue("activity.queue", true);
+        return new Queue(queue, true);
+    }
+
+    //Binding above queue to exchange,
+    //we are also getting exchange and binding it to routing key
+    @Bean
+    public DirectExchange activityExchange(){
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Binding activityBinding(Queue activityQueue, DirectExchange activityExchange){
+        return BindingBuilder.bind(activityQueue).to(activityExchange).with(routingKey);
     }
 
     @Bean
