ccf7f358003fa879b013521ef11b7c6934a1bfb3
Davinder47 <davinder9730@gmail.com>
2026-04-27 17:06:57 +0530

    Pasted the same RabbitMQ configuration file in aiservice config model for RabbitMQ configuration

---
+++ aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
@@ -0,0 +1,61 @@
+package com.fitness.aiservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue activityQueue() {
+        return new Queue(queue, true);
+    }
+
+    //Binding above queue to exchange,
+    //we are also getting exchange and binding it to routing key
+    @Bean
+    public DirectExchange activityExchange(){
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Binding activityBinding(Queue activityQueue, DirectExchange activityExchange){
+        return BindingBuilder.bind(activityQueue).to(activityExchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); // ✅ fixes LocalDateTime
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
